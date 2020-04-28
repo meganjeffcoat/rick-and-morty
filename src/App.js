c4e57ef04889617394194609bdaae375df0750b3
Megan <jeffcoat.megan@gmail.com>
2020-04-28 17:14:43 -0500

    characters render

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import WelcomePage from './components/WelcomePage';
+import CharList from './components/CharList';
 
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Header />
-        <Route exact path='/' component={WelcomePage} />
+        <CharList />
+        {/* <Header />
+        <Route exact path='/' component={WelcomePage} /> */}
       </main>
     </div>
   );
--- src/components/CharList.js
+++ src/components/CharList.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import CharCard from './CharCard';
 
 export default function CharList() {
 
-    useEffect(() => {
+    const [chars, setChars] = useState([]);
 
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character/")
+        .then(res => {
+            console.log(res.data.results);
+            setChars(res.data.results);
+        })
     }, [])
 
     return (
         <section>
             <h2>Char List</h2>
+            <Link to={'/'}>
+                Home
+            </Link>
+            {chars.map(char => {
+                return <CharCard key={char.id} character={char} />
+            })}
         </section>
     );
 }
