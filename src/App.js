96afcfa75a33c978029782cab86baaa2050aa886
Megan <jeffcoat.megan@gmail.com>
2020-04-28 16:51:15 -0500

    welcome page route in app and fixed img and welcomepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import Header from './components/Header';
+import WelcomePage from './components/WelcomePage';
+
 
 function App() {
   return (
     <div className="App">
       <main>
         <Header />
+        <Route exact path='/' component={WelcomePage} />
       </main>
     </div>
   );
