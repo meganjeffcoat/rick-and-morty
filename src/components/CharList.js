36aaeef102226d5ecc57635f1ee9ec8898b6d1ff
Megan <jeffcoat.megan@gmail.com>
2020-04-28 16:37:32 -0500

    componenets set up

---
+++ src/components/CharList.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+export default function CharList() {
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <section>
+            <h2>Char List</h2>
+        </section>
+    );
+}
