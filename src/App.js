b3f68ef11fcfc8f3cf0cc73815b6ee009fdc6a5e
Megan <jeffcoat.megan@gmail.com>
2020-04-29 12:28:34 -0500

    basic info on location and nav set

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import WelcomePage from './components/WelcomePage';
+import Navigation from './components/Navigation';
 import CharList from './components/CharList';
+import LocationsList from './components/LocationsList';
 
 
 
@@ -13,8 +15,10 @@ function App() {
     <div className="App">
       <main>
         <Header />
+        <Navigation />
         <Route exact path='/' component={WelcomePage} />
         <Route path='/characters' component={CharList} />
+        <Route path='/locations' component={LocationsList} />
         
       </main>
     </div>
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import LocationCard from './LocationCard';
 
 export default function LocationsList() {
+
+    const [location, setLocation] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('https://rickandmortyapi.com/api/location/')
+            .then(res => {
+                console.log('locaion', res.data.results);
+                setLocation(res.data.results);
+            })
+            .catch(err =>{
+                console.log(err)
+            })
+    }, []);
     return(
         <section>
-            <p>Locations List</p>
+            {location.map(loc => {
+                return <LocationCard key={loc.id} loc={loc} />
+            })}
         </section>
     )
 }
