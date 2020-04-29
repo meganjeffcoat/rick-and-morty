e53aae8feeb3edfdf6f4120ccbd32eda2d4815f3
Megan <jeffcoat.megan@gmail.com>
2020-04-29 18:18:19 -0500

    styling of nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default function Navigaton(){
-    return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to={'/characters'}>Characters</Link>
-            <Link to={'/locations'}>Locations</Link>
-            <Link to={'/episodes'}>Episodes</Link>
-        </div>
-    )
+const Nav = styled.section`
+  background-color: #2b343e;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding-bottom: 1.5%;
+`;
+
+const StyledLink = styled(Link)`
+  color: #c2d146;
+  text-decoration: none;
+  font-size: 1em;
+
+  &:hover {
+      text-decoration: none;
+      color: #5AADC9;
+  }
+`;
+
+export default function Navigaton() {
+  return (
+    <Nav>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to={"/characters"}>Characters</StyledLink>
+      <StyledLink to={"/locations"}>Locations</StyledLink>
+      <StyledLink to={"/episodes"}>Episodes</StyledLink>
+    </Nav>
+  );
 }
