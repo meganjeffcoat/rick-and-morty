8dffb61dea0bce94d9ec5b1942c531b1188076f8
Megan <jeffcoat.megan@gmail.com>
2020-04-29 18:02:56 -0500

    styling of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,36 @@ import styled from 'styled-components';
 
 import logo from '../logo.png';
 
+const Section = styled.section`
+    background-color: #2b343e;
+    font-family: "Poppins", sans-serif;
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
 
+    img{
+        width: 50%;
+        margin-left: 25%;
+        margin-top: 2%;
+    }
+
+    h1 {
+        font-size: 2.5em;
+        color: #5AADC9;
+        margin-left: 38%;
+        margin-top: -1%;
+    }
+`;
+
+const Title = styled.div`
+    display: flex;
+`;
 
 export default function Header() {
     return (
-        <header>
+        <Section>
             <img src={logo} />
             <h1>Fan Page</h1>
-        </header>
+        </Section>
     )
 }
